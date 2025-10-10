4b06fc485e762890c47a3e46baf2de3c461c295f
Ben Swift <ben@benswift.me>
2025-10-11 10:10:40 +1100

    update deps
    add usage rules as well

--- mix.exs
+++ mix.exs
@@ -3,20 +3,7 @@ defmodule HendrixHomeostat.MixProject do
 
   @app :hendrix_homeostat
   @version "0.1.0"
-  @all_targets [
-    :rpi,
-    :rpi0,
-    :rpi2,
-    :rpi3,
-    :rpi3a,
-    :rpi4,
-    :rpi5,
-    :bbb,
-    :osd32mp1,
-    :x86_64,
-    :grisp2,
-    :mangopi_mq_pro
-  ]
+  @all_targets [:rpi5]
 
   def project do
     [
@@ -43,35 +30,22 @@ defmodule HendrixHomeostat.MixProject do
   defp deps do
     [
       # Dependencies for all targets
-      {:nerves, "~> 1.10", runtime: false},
-      {:shoehorn, "~> 0.9.1"},
-      {:ring_logger, "~> 0.11.0"},
-      {:toolshed, "~> 0.4.0"},
+      {:nerves, "~> 1.11", runtime: false},
+      {:shoehorn, "~> 0.9.3"},
+      {:ring_logger, "~> 0.11.4"},
+      {:toolshed, "~> 0.4.2"},
+      {:igniter, "~> 0.6"},
+      {:usage_rules, "~> 0.1"},
 
       # Allow Nerves.Runtime on host to support development, testing and CI.
       # See config/host.exs for usage.
-      {:nerves_runtime, "~> 0.13.0"},
+      {:nerves_runtime, "~> 0.13.9"},
 
       # Dependencies for all targets except :host
       {:nerves_pack, "~> 0.7.1", targets: @all_targets},
 
       # Dependencies for specific targets
-      # NOTE: It's generally low risk and recommended to follow minor version
-      # bumps to Nerves systems. Since these include Linux kernel and Erlang
-      # version updates, please review their release notes in case
-      # changes to your application are needed.
-      {:nerves_system_rpi, "~> 1.24", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.24", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.24", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.24", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.24", runtime: false, targets: :rpi3a},
-      {:nerves_system_rpi4, "~> 1.24", runtime: false, targets: :rpi4},
-      {:nerves_system_rpi5_audio, "~> 0.6", runtime: false, targets: :rpi5, github: "ANUcybernetics/nerves_system_rpi5_audio", nerves: [compile: true]},
-      {:nerves_system_bbb, "~> 2.19", runtime: false, targets: :bbb},
-      {:nerves_system_osd32mp1, "~> 0.15", runtime: false, targets: :osd32mp1},
-      {:nerves_system_x86_64, "~> 1.24", runtime: false, targets: :x86_64},
-      {:nerves_system_grisp2, "~> 0.8", runtime: false, targets: :grisp2},
-      {:nerves_system_mangopi_mq_pro, "~> 0.6", runtime: false, targets: :mangopi_mq_pro}
+      {:nerves_system_rpi5_audio, "~> 0.6", runtime: false, targets: :rpi5, github: "ANUcybernetics/nerves_system_rpi5_audio", nerves: [compile: true]}
     ]
   end
 
