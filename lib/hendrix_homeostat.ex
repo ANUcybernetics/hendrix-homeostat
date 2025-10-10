af2d2d8dce5be5e8f9cdaf4a52de5d34ecf328f8
Ben Swift <ben@benswift.me>
2025-10-10 15:59:22 +1100

    initial commit
    just the result of following the nerves instructions

---
+++ lib/hendrix_homeostat.ex
@@ -0,0 +1,18 @@
+defmodule HendrixHomeostat do
+  @moduledoc """
+  Documentation for `HendrixHomeostat`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HendrixHomeostat.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hendrix_homeostat_test.exs
@@ -0,0 +1,8 @@
+defmodule HendrixHomeostatTest do
+  use ExUnit.Case
+  doctest HendrixHomeostat
+
+  test "greets the world" do
+    assert HendrixHomeostat.hello() == :world
+  end
+end
